db316678185d57479f246d1073ed4713e627505e
Melvin <melvin0008@gmail.com>
2015-03-01 14:25:02 +0530

    added the main templates and routing options

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app=angular.module('ionic-socketio-chat-client', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,20 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider)
+{
+
+  $stateProvider
+  .state('chat', {
+    url: "/chat",
+    templateUrl: "templates/chat.html"
+  }).
+  .state('login', {
+    url: "/login",
+    templateUrl: "templates/login.html"
+  })
+  
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+}
