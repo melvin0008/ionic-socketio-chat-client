a997b0eeaeb94ad09c863caed22f487cf53da288
Melvin <melvin0008@gmail.com>
2015-03-18 18:20:52 +0530

    add a directive

---
+++ www/js/directives.js
@@ -0,0 +1,13 @@
+app.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                        scope.$apply(function(){
+                                scope.$eval(attrs.ngEnter);
+                        });
+                        
+                        event.preventDefault();
+                }
+            });
+        };
+});
