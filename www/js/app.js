9d9c126fa43a4deafa059c21cffd13002d883f9d
Melvin <melvin0008@gmail.com>
2015-03-25 18:28:41 +0530

    changed bottom to fit on ios mobiles

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,5 @@
 // Ionic Socket IO app
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var app=angular.module('ionic-socketio-chat-client', ['ionic', 'ngSanitize','btford.socket-io'])
 
 .run(function($ionicPlatform) {
